ea1960b2145cee6e9d17a16bdd407071ffdd1691
agluzhin <agluzhin@gmail.com>
2025-12-09 19:20:03 +0300

    feat (NotificationService/ShortLinkService): add entities logic.

--- src/main/java/org/agluzhin/services/NotificationService.java
+++ src/main/java/org/agluzhin/services/NotificationService.java
@@ -1,4 +1,13 @@
 package org.agluzhin.services;
 
+import org.agluzhin.entities.ShortLink;
+
 public class NotificationService {
+    public void notifyUser(ShortLink link) throws Exception {
+        if (link.getClickCount() >= link.getClickLimit()) {
+            throw new Exception(String.format("ссылка '%s' недоступна - лимит переходов исчерпан", link.getShortCode()));
+        } else {
+            throw new Exception(String.format("ссылка '%s' недоступна - срок жизни истек.", link.getShortCode()));
+        }
+    }
 }
