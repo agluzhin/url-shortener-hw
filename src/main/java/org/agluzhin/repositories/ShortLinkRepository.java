c12dd1d6a8191f25e25e41329d34ceabb9f03e47
agluzhin <agluzhin@gmail.com>
2025-12-10 13:36:58 +0300

    refactor (ShortLinkRepository): new logic of work (use userId as a key in storage), rename method from "getLinkByCode" to "getLink".

--- src/main/java/org/agluzhin/repositories/ShortLinkRepository.java
+++ src/main/java/org/agluzhin/repositories/ShortLinkRepository.java
@@ -6,21 +6,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ShortLinkRepository {
-    private final Map<String, ShortLink> LINKS = new HashMap<>();
+    private final Map<String, Map<String, ShortLink>> LINKS = new HashMap<>();
 
-    public void addLink(ShortLink link) {
-        LINKS.put(link.getShortCode(), link);
+    public void addLink(String userId, ShortLink link) {
+        Map<String, ShortLink> userLinks = LINKS.get(userId);
+        userLinks.put(link.getShortUrl(), link);
     }
 
-    public void deleteLink(String code) {
-        LINKS.remove(code);
+    public void deleteLink(String userId, String shortUrl) {
+        Map<String, ShortLink> userLinks = LINKS.get(userId);
+        userLinks.remove(shortUrl);
     }
 
-    public ShortLink getLinkByCode(String code) {
-        return LINKS.get(code);
+    public ShortLink getLink(String userId, String shortUrl) {
+        Map<String, ShortLink> userLinks = LINKS.get(userId);
+        return userLinks.get(shortUrl);
     }
 
-    public Map<String, ShortLink> getAllLinks() {
+    public Map<String, Map<String, ShortLink>> getAllLinks() {
         return LINKS;
     }
 }
