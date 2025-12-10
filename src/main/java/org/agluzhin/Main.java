b52d8959f56d5c9ebc0ede181a846dc5c19160eb
agluzhin <agluzhin@gmail.com>
2025-12-10 14:21:06 +0300

    feat (Main): add exception handling (with try...catch).

--- src/main/java/org/agluzhin/Main.java
+++ src/main/java/org/agluzhin/Main.java
@@ -27,34 +27,38 @@ public class Main {
             System.out.print("Выберите действие: ");
             int userChoice = Integer.parseInt(scanner.nextLine());
 
-            switch(userChoice) {
-                case 1:
-                    System.out.print("\nВведите оригинальную ссылку: ");
-                    String originalUrl = scanner.nextLine();
-                    System.out.print("Введите лимит переходов: ");
-                    int clickLimit = Integer.parseInt(scanner.nextLine());
-                    System.out.print("Введите время жизни ссылки в секундах: ");
-                    long lifeLimit = Long.parseLong(scanner.nextLine());
+            try {
+                switch(userChoice) {
+                    case 1:
+                        System.out.print("\nВведите оригинальную ссылку: ");
+                        String originalUrl = scanner.nextLine();
+                        System.out.print("Введите лимит переходов: ");
+                        int clickLimit = Integer.parseInt(scanner.nextLine());
+                        System.out.print("Введите время жизни ссылки в секундах: ");
+                        long lifeLimit = Long.parseLong(scanner.nextLine());
 
-                    ShortLink shortLink = shortLinkService.createShortLink(originalUrl, user, clickLimit, lifeLimit);
-                    System.out.printf("Ваша короткая ссылка: %s%n", shortLink.getShortUrl());
-                    break;
-                case 2:
-                    System.out.print("\nВведите короткую ссылку: ");
-                    String shortUrl = scanner.nextLine();
-                    shortLinkService.visitLink(user.getId(), shortUrl);
-                    break;
-                case 3:
-                    System.out.println();
-                    System.out.printf("Все короткие ссылки для пользователя %s:", user.getId());
-                    for (String url : shortLinkRepository.getAllUserLinks(user.getId()).keySet()) {
-                        System.out.println(url);
-                    }
-                    break;
-                case 4:
-                    System.out.println("\nДо свидания!");
-                    System.exit(0);
-                    break;
+                        ShortLink shortLink = shortLinkService.createShortLink(originalUrl, user, clickLimit, lifeLimit);
+                        System.out.printf("Ваша короткая ссылка: %s%n", shortLink.getShortUrl());
+                        break;
+                    case 2:
+                        System.out.print("\nВведите короткую ссылку: ");
+                        String shortUrl = scanner.nextLine();
+                        shortLinkService.visitLink(user.getId(), shortUrl);
+                        break;
+                    case 3:
+                        System.out.println();
+                        System.out.printf("Все короткие ссылки для пользователя %s:", user.getId());
+                        for (String url : shortLinkRepository.getAllUserLinks(user.getId()).keySet()) {
+                            System.out.println(url);
+                        }
+                        break;
+                    case 4:
+                        System.out.println("\nДо свидания!");
+                        System.exit(0);
+                        break;
+                }
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
             }
         }
     }
