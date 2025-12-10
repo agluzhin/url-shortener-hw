4614c38592f9aa05997b3bb1e40771e78585db8d
agluzhin <agluzhin@gmail.com>
2025-12-10 14:14:42 +0300

    feat (ShortLinkRepository): add data validation and exception throwing.

--- src/main/java/org/agluzhin/repositories/ShortLinkRepository.java
+++ src/main/java/org/agluzhin/repositories/ShortLinkRepository.java
@@ -1,29 +1,98 @@
 package org.agluzhin.repositories;
 
 import org.agluzhin.entities.ShortLink;
+import org.agluzhin.utils.ValidationUtil;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class ShortLinkRepository {
-    private final Map<String, Map<String, ShortLink>> LINKS = new HashMap<>();
+    private final Map<String, Map<String, ShortLink>> SHORT_LINKS = new HashMap<>();
 
-    public void addLink(String userId, ShortLink link) {
-        Map<String, ShortLink> userLinks = LINKS.get(userId);
-        userLinks.put(link.getShortUrl(), link);
+    public void addLink(String userId, ShortLink shortLink) {
+        ValidationUtil.requireNonNullOrEmpty("userId", userId);
+        ValidationUtil.requireNonNull("shortLink", shortLink);
+
+        Map<String, ShortLink> userShortLink;
+        if (SHORT_LINKS.containsKey(userId)) {
+            userShortLink = SHORT_LINKS.get(userId);
+            userShortLink.put(shortLink.getShortUrl(), shortLink);
+        } else {
+            userShortLink = new HashMap<>();
+            userShortLink.put(shortLink.getShortUrl(), shortLink);
+            SHORT_LINKS.put(userId, userShortLink);
+        }
     }
 
-    public void deleteLink(String userId, String shortUrl) {
-        Map<String, ShortLink> userLinks = LINKS.get(userId);
-        userLinks.remove(shortUrl);
+    public void deleteLink(String userId, String shortUrl) throws NoSuchElementException {
+        ValidationUtil.requireNonNullOrEmpty("userId", userId);
+        ValidationUtil.requireNonNullOrEmpty("shortUrl", shortUrl);
+
+        Map<String, ShortLink> userLinks;
+        if (SHORT_LINKS.containsKey(userId)) {
+            userLinks = SHORT_LINKS.get(userId);
+            if (userLinks.containsKey(shortUrl)) {
+                userLinks.remove(shortUrl);
+            } else {
+                throw new NoSuchElementException(
+                        String.format(
+                                "ссылка '%s' у пользователя с id '%s' не найдена",
+                                shortUrl,
+                                userId
+                        )
+                );
+            }
+        } else {
+            throw new NoSuchElementException(
+                    String.format(
+                            "пользователь с id '%s' не найден",
+                            userId
+                    )
+            );
+        }
     }
 
-    public ShortLink getLink(String userId, String shortUrl) {
-        Map<String, ShortLink> userLinks = LINKS.get(userId);
-        return userLinks.get(shortUrl);
+    public ShortLink getLink(String userId, String shortUrl) throws NoSuchElementException {
+        ValidationUtil.requireNonNullOrEmpty("userId", userId);
+        ValidationUtil.requireNonNullOrEmpty("shortUrl", shortUrl);
+
+        Map<String, ShortLink> userLinks;
+        if (SHORT_LINKS.containsKey(userId)) {
+            userLinks = SHORT_LINKS.get(userId);
+            if (userLinks.containsKey(shortUrl)) {
+                return userLinks.get(shortUrl);
+            } else {
+                throw new NoSuchElementException(
+                        String.format(
+                                "ссылка '%s' у пользователя с id '%s' не найдена",
+                                shortUrl,
+                                userId
+                        )
+                );
+            }
+        } else {
+            throw new NoSuchElementException(
+                    String.format(
+                            "пользователь с id '%s' не найден",
+                            userId
+                    )
+            );
+        }
     }
 
-    public Map<String, ShortLink> getAllUserLinks(String userId) {
-        return LINKS.get(userId);
+    public Map<String, ShortLink> getAllUserLinks(String userId) throws NoSuchElementException {
+        ValidationUtil.requireNonNullOrEmpty("userId", userId);
+
+        if (SHORT_LINKS.containsKey(userId)) {
+            return SHORT_LINKS.get(userId);
+        } else {
+            throw new NoSuchElementException(
+                    String.format(
+                            "пользователь с id '%s' не найден",
+                            userId
+                    )
+            );
+        }
     }
 }
